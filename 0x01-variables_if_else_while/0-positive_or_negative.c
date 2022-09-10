53d95c49831b21447b7073293b36a3ac8205d97b
Allan Agaba <aglanx@gmail.com>
2022-09-10 00:03:41 +0000

    Task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,9 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -26,4 +28,4 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	return (0);
-}
+} 
