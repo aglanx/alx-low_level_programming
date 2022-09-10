b63fb606aa87700866d91e324f93183df5c8ec63
Allan Agaba <aglanx@gmail.com>
2022-09-10 01:56:42 +0000

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,12 +2,13 @@
 
 /**
  * main - prints the alphabet in lowercase
- * 
+ *
  * Return: Alwyays 0.
  */
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
