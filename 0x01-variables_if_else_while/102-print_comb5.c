8cf11a0913012ec07bfc7ab7d6293131b8861927
Allan Agaba <aglanx@gmail.com>
2022-09-11 01:33:27 +0000

    Task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,23 @@
  */
 int main(void)
 {
-	int num;
+	int num1, num2;
 
-	for (num = 10; num <= 99; num++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		putchar((num % 100) + '0');
-		if (num == 99)
-			continue;
-		putchar(',');
-		putchar(' ');
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
