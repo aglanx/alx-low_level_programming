89e7a578fe38fb9861cf363cd469ac9171abb6ae
Allan Agaba <aglanx@gmail.com>
2022-09-14 20:38:50 +0000

    Task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j<=n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				result = (i * j);
 
@@ -29,7 +29,7 @@ void print_times_table(int n)
 				else if (result >= 10 && result < 100)
 				{
 					_putchar(' ');
-					_putchar((result / 10) + '0';
+					_putchar((result / 10) + '0');
 					_putchar((result % 10) + '0');
 				}
 				else if (result >= 100 && j != 0)
