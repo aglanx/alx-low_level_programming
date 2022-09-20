75defacde01960a63082ed18f5c2b56a50a32c32
Allan Agaba <aglanx@gmail.com>
2022-09-20 22:04:38 +0000

    Task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[index])
+	while (src[i])
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
+		i++;
 	}
 	return (dest);
 }
