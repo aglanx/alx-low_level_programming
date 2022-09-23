f42ff251224dc48499075e07d0111191bbfe8a92
Allan Agaba <aglanx@gmail.com>
2022-09-23 21:23:46 +0000

    Task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 
 /**
  *_strcat - concatenates  the string pointed to by @src to
- *the end of the string pointed to by @dest
+ * the end of the string pointed to by @dest
  *@dest: String that will be appended
  *@src: String to be concatenated upon
  *
@@ -11,14 +11,11 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int index = 0, dest_len = 0;
 
-int index = 0, dest_len = 0;
-
-while (dest[index++])
-	dest_len++;
-
-for (index = 0; src[index]; index++)
-	dest[dest_len++] = src[index];
-
-return (dest);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
